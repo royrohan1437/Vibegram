fb3cb597a1ac745356e29e41dbda93e0816b8347
Rohan Roy <roy.rohan14378.com>
2025-10-22 12:40:20 +0530

    Initial commit: added backend structure with controllers, models, middleware, and routes setup

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,32 @@
+import {StreamChat} from "stream-chat"
+import "dotenv/config"
+
+const apiKey = process.env.STREAM_API_KEY;
+const apiSecret = process.env.STREAM_SECRET_KEY;
+
+if(!apiKey || !apiSecret){
+    console.error("Stream API key or Secret is missing");
+}
+
+const streamClient = StreamChat.getInstance(apiKey, apiSecret);
+
+export const upsertStreamUser = async (userData) => {
+    try {
+        await streamClient.upsertUsers([userData]);
+        return userData
+    } catch (error) {
+        console.error("Error upserting Stream user: ",  error );
+    }
+}
+
+
+//TODO : Do it later 
+export const generateStreamToken = (userId) => {
+    try {
+        //ensure userID is a string 
+        const userIdStr = userId.toString();
+        return streamClient.createToken(userIdStr)
+    } catch (error) {
+        console.error("Error generating Stream Token:", error);
+    }
+};
---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Route, Routes } from "react-router"
+import HomePage from "./pages/HomePage.jsx"
+import SignUpPage from "./pages/SignUpPage.jsx"
+import LoginPage from "./pages/LoginPage.jsx"
+import NotificationsPage from "./pages/NotificationsPage.jsx"
+import CallPage from "./pages/CallPage.jsx"
+import ChatPage from "./pages/ChatPage.jsx"   
+import OnboardingPage from "./pages/OnboardingPage.jsx"
+import {Toaster} from "react-hot-toast"
+
+const App = () => {
+  return (
+    <div className= "h-screen " data-theme="night">
+    <Routes>
+      <Route path = "/" element = {<HomePage/>}/>
+      <Route path = "/signup" element = {<SignUpPage/>}/>
+      <Route path = "/login" element = {<LoginPage/>}/>
+      <Route path = "/notifications" element = {<NotificationsPage/>}/>
+      <Route path = "/call" element = {<CallPage/>}/>
+      <Route path = "/chat" element = {<ChatPage/>}/>
+      <Route path = "/onboarding" element = {<OnboardingPage/>}/>
+    </Routes>
+    <Toaster/>
+    </div>
+  );
+};
+
+export default App
